20b56f71d1c6af6b0e643a66eccc166ea1f0c44e
obaddabo <euphoria214rue@gmail.com>
2023-07-14 00:04:58 +0100

    multer

--- index.js
+++ index.js
@@ -1,7 +1,27 @@
 const express = require('express');
 const app = express(); 
 const openai = require('openai')
+const tonal = require('tonal');
+const multer = require('multer');
+const uid = require('uid');
 
+
+
+app.set('view engine', 'ejs');
+app.set('views', 'views');
+
+const multer = require('multer');
+
+const storage = multer.diskStorage({
+  destination: function (req, file, cb) {
+    cb(null, 'uploads/');
+  },
+  filename: function (req, file, cb) {
+    cb(null, file.originalname);
+  }
+});
+
+const upload = multer({ storage: storage });
 
 
 
@@ -10,7 +30,12 @@ app.get('/', (req,res)=>{
 
 res.send('hello world');
 
-})
+});
+
+app.post('/upload', upload.single('audio'), (req, res) => {
+  res.send('Audio file uploaded successfully');
+});
+
 
 app.listen(3000, ()=> console.log('hello world'));
 
