ed2a5266546512e9fcb37b26226bd4dbc5ca2e28
obaddabo <euphoria214rue@gmail.com>
2023-07-16 01:30:56 +0100

    updatez

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const app = express(); 
 const openai = require('openai')
-const formidable = require('formidable')
+const multer = require('multer')
 const uid = require('uid');
 const {Essentia , EssentiaWASM} = require ('essentia.js')
 const essentia = new Essentia(EssentiaWASM);
@@ -12,20 +12,50 @@ const port = process.env.PORT ;
 
 
 
-
+// setting view engine , view folder and public folder
 app.set('view engine', 'ejs');
 app.set('views', 'views');
 app.set(express.static('public'));
 
 
+//create a new multer instance
+
+const upload = multer({ dest : 'uploads/'});
+
+
+app.get('/' , (req,res)=>{
+	
+	res.render('index.ejs');
+
+
+}
+
+//route to handle file uploads
+
+app.post('/uploads/beat', upload.single('audio'), (req,res)=>{
+
+// get the uploaded file
+
+const file = req.file;
+
+// save the file to the uploads folder
+
+fs.renameSync(file.path, `uploads/${file.originalname}`);
+
+
+});
+
+
+
+//start the server
+
+app.listen(3000, ()=>{
+console.log('server started on ' + port );
+})
 
 
 
 
 
-const decodeAudio = async (filepath: string) => {
-    const buffer = fs.readFileSync(filepath);
-    const audio = await decode(buffer);
-    const audioVector = essentia.arrayToVector(audio._channelData[0]);
-    return audioVector;
-}; 
+
+
