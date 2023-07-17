d1617637cc6fd0beee26bad8bf54c61f7692c1ea
obaddabo <euphoria214rue@gmail.com>
2023-07-17 21:28:12 +0100

    updates

--- index.js
+++ index.js
@@ -1,13 +1,13 @@
 //in package.json type is set to module so import statement is used
 
-import 'express', {NextFunction} from "express"
+import express, {NextFunction} from "express"
 const app = express(); 
 import  openai  from 'openai'
 import  multer from 'multer'
 import {Essentia , EssentiaWASM} from 'essentia.js'
 const essentia = new Essentia(EssentiaWASM);
 import fs from 'fs'
-import decodeAudio from 'audio-decode'
+import decode from 'audio-decode'
 const port = process.env.PORT ;
 
 
@@ -19,11 +19,40 @@ app.set('view engine', 'ejs');
 app.set('views', 'views');
 app.set(express.static('public'));
 
-// 
+// specify dir and filename
+const storage = multer.diskStorage({
+  destination: function (req, file, cb) {
+    // Specify the directory where you want to save the uploaded files
+    cb(null, 'uploads/');
+  },
+  filename: function (req, file, cb) {
+    // Generate a unique name for the file
+    cb(null, Date.now() + '-' + file.originalname);
+  }
+});
+
 
 //create a new multer instance
 
-const upload = multer({ dest : 'uploads/'});
+const upload = multer({ storage : storage,
+	limits : { 
+	filesize : 10 * 1024 * 1024 , } ,
+});
+
+// decoding 
+
+
+const decodeAudio  = async ( filepath : string ) =>{
+
+	const buffer = readFileSync(filepath)
+	const audio = decode(buffer)
+	const audioVector = essentia.arrayToVector(audio.channel._channelData[0])
+	return audioVector;
+
+
+
+}
+
 
 
 app.get('/' , (req,res)=>{
@@ -35,26 +64,19 @@ app.get('/' , (req,res)=>{
 
 //route to handle file uploads
 
-app.post('/uploads/beat', upload.single('audio'), (req,res)=>{
+app.post('/uploads/beat', upload.single('audio'),async (req,res)=>{
 
 // get the uploaded file
 
 const file = req.file;
 
 const path = file.path;
-// save the file to the uploads folder
 
-fs.renameSync(path, `uploads/${file.originalname}`);
+//decode the audio
+
+const data = await  decodeAudio(path)
 
 
-//using audio-decode to decode the audio
- const decodedaudio = async(path)=>{
-
-const buffer = fs.readFileSync(file.path);
-const Audio = await decode(buffer)
-const audioVector = essentia.arrayToVector(audio._channelData[0]);
-return audioVector;
-}
 
 
 
