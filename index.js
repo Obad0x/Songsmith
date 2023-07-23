addbb9392166306f37c175aba65f5c8cafb0f640
obaddabo <euphoria214rue@gmail.com>
2023-07-23 02:51:40 +0100

    openaiupdates

--- index.js
+++ index.js
@@ -10,11 +10,9 @@ import dotenv from 'dotenv'
 dotenv.config();
 import { Configuration, OpenAIApi } from "openai";
 const configuration = new Configuration({
-    organization: "org-fO2tNR9Slg1D61b1t1j1wPe6",
     apiKey: process.env.OPENAI_API_KEY,
 });
 const openai = new OpenAIApi(configuration);
-const response = await openai.listEngines();
  
 
 
@@ -96,20 +94,26 @@ const data = await  decodeAudio(path)
     const loudness = essentia.DynamicComplexity(data).loudness;
     const tempo = essentia.PercivalBpmEstimator(data).bpm;
  
-try {
-  const completion = await openai.createCompletion({
-    model: "text-davinci-003",
-    prompt: prompt,
-  });
-  console.log(completion.data.choices[0].text);
-} catch (error) {
-  if (error.response) {
+
+try{
+
+	const response = await openai.createCompletion({
+	  model: "text-davinci-003",
+	  prompt: "Say this is a test",
+ 	 max_tokens: 7,
+ 	 temperature: 0,
+});
+}
+catch (err){
+
+	if (error.response) {
     console.log(error.response.status);
     console.log(error.response.data);
   } else {
     console.log(error.message);
-  }
 }
+
+
 
 });
 
